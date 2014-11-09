8ab9d184b5cf3f6c2869108edcb1e161684374c7
Jacky Liang <jackyliang@berkeley.edu>
2014-11-08 19:12:38 -0800

    bower package setup

--- index.js
+++
@@ -1,7 +0,0 @@
-function OCRImage(image){
-	var canvas = document.createElement('canvas')
-	canvas.width = image.naturalWidth;
-	canvas.height = image.naturalHeight;
-	canvas.getContext('2d').drawImage(image, 0, 0)
-	return OCRAD(canvas)
-}
